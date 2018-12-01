cae2f5021beedbedad899e908f92075276aaaab5
yeshab7777 <yessezaolohabib@yahoo.fr>
2018-12-01 14:24:43 +0100

    + update

---
+++ InitializrSpringbootProject/src/main/java/dsc/pwa/emusic/springbootwebsocket/SpringBootWebSocketApplication.java
@@ -0,0 +1,12 @@
+package dsc.pwa.emusic.springbootwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSocketApplication.class, args);
+	}
+}
